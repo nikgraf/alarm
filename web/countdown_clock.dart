3ecd84761ac31d72ce9cde4b59200e47b784d0c4
Nik Graf <nik@blossom.io>
2013-07-13 20:51:48 -0700

    update the time left

--- web/countdown_clock.dart
+++ web/countdown_clock.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
 import 'package:polymer/polymer.dart';
+import 'package:observe/observe.dart';
 
-class CountdownClock extends CustomElement {
+class CountdownClock extends CustomElement with ObservableMixin {
 
-  var timeLeft = new Duration(seconds: 72);
+  static final oneSecond = new Duration(seconds: 1);
+  @observable var timeLeft = new Duration(seconds: 72);
+
+  /*
+   * Initialize the timer who updates the duration periodically
+   * when the component has been added to the DOM.
+   */
+  void inserted() {
+    new Timer.periodic(oneSecond, _updateTimeLeft);
+  }
+
+  /*
+   * Update timeLeft until 0 seconds left.
+   */
+  void _updateTimeLeft(Timer _) {
+    if (timeLeft > new Duration(seconds: 0)) {
+      timeLeft = timeLeft - oneSecond;
+    }
+  }
+
 }
